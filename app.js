f06065e52b5f77b2be3156a5dcd8677090940e54
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2024-09-08 00:00:24 -0500

    still has some bugs i need to fix

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-let ageInput = document.getElementById("age");
+let ageInput = document.getElementById("birthYear");
 console.log(ageInput);
 
 let ageButton = document.getElementById("checkAge");
 console.log(ageButton.value);
 
-let displayElement = document.querySelector(".col1 h1")
-console.log(displayElement.innerHTML)
+let displayElement = document.querySelector(".col1 h3")
+console.log(displayElement.innerHTML);
 
-let age = "";
+// img1 = document.querySelector(".img1 img");
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+console.log(img1);
+console.log(img2);
 
+let currentYear = new Date().getFullYear() 
+console.log(currentYear);
+let birthYear = "";
+
+let nAn = "abcdefghijklmnopqrstuvwxyz`~!@#$%^&*()-_=+[{]}:;$|'<,>.?/'\`";
+console.log(nAn);
 ageInput.addEventListener("input", () => 
 {
-    age = ageInput.value;
-    console.log(age)
+    
+    // birthYear = ageInput.value;
+    // console.log(birthYear)
 })
 
 ageButton.addEventListener("click", () =>
 {
-    if (age < 18) {displayElement.innerHTML = "you are a child. NO ALCOHOLIC BEVERAGES FOR YOU!";}
-    else {displayElement.innerHTML = "You are an adult. Welcome to the grown up club!";}
+    if (birthYear > currentYear - 18)
+        {
+            displayElement.innerHTML = "You are a MINOR. You must be 18 to get in.";
+            img1.classList.remove("d-none");
+        }
+    else
+    {
+        displayElement.innerHTML = "You are an adult. Welcome to the grown up club!";
+        img2.classList.remove("d-none");
+    }
 })
